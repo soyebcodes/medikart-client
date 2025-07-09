e225ddc69593e19a293094e3d7178799e6f86132
soyebcodes <soyeb10191@gmail.com>
2025-07-09 23:59:13 +0600

    add sidebar components for different role

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router";
-import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
+import UserSidebar from "../components/Dashboard/UserSidebar";
+import SellerSidebar from "../components/Dashboard/SellerSidebar";
+import AdminSidebar from "../components/Dashboard/AdminSidebar";
+import { useAuth } from "../hooks/useAuth";
 
 const DashboardLayout = () => {
+  const { user } = useAuth();
+
+  let SidebarComponent = UserSidebar; // default
+
+  if (user?.role === "admin") SidebarComponent = AdminSidebar;
+  else if (user?.role === "seller") SidebarComponent = SellerSidebar;
+
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1 bg-base-200 p-6 overflow-y-auto">
+      <SidebarComponent />
+      <main className="flex-1 bg-base-200 p-6 overflow-y-auto">
         <Outlet />
-      </div>
+      </main>
     </div>
   );
 };
