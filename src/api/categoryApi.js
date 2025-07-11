29ec7dc581de2b0f7c6fec68da7c14b9096e0616
soyebcodes <soyeb10191@gmail.com>
2025-07-11 13:47:18 +0600

    Fix cloudinary upload issue

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -8,19 +8,14 @@ export const fetchCategories = async () => {
 };
 
 // For createCategory and updateCategory, expect 'category' to be FormData
-export const createCategory = async (category) => {
-  const { data } = await axios.post(`${BASE_URL}/api/categories`, category, {
-    // Do NOT set Content-Type here; let Axios handle multipart/form-data boundary
-  });
-  return data;
+export const createCategory = async (formData) => {
+  return (await axios.post(`${BASE_URL}/api/categories`, formData)).data;
 };
 
-export const updateCategory = async (id, category) => {
-  const { data } = await axios.put(`${BASE_URL}/api/categories/${id}`, category, {
-    // Same: no manual content-type header
-  });
-  return data;
+export const updateCategory = async (id, formData) => {
+  return (await axios.put(`${BASE_URL}/api/categories/${id}`, formData)).data;
 };
+
 
 export const deleteCategory = async (id) => {
   const { data } = await axios.delete(`${BASE_URL}/api/categories/${id}`);
